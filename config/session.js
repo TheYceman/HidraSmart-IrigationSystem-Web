9cd233b338300142fef71eb86c6e12ce8df706d3
Mª Ángeles López <mariaangeles.lopez@hidralab.com>
2024-04-05 16:11:22 +0200

    Corrección de errores

--- config/session.js
+++ config/session.js
@@ -12,7 +12,7 @@ function createSessionStore() {
     user: "telemedida_alcazar",
     password: "Hidra2023Alcazar",
     port: 3306,
-    database: ddbb, //"aplicaciones_web"
+    database: "aplicaciones_web", //"aplicaciones_web"
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
