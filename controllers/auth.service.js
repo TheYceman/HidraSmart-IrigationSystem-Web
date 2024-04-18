3a8317f1ad2eca8c5269e5fb3ee9d0cd8fe0ce4b
Mª Ángeles López <mariaangeles.lopez@hidralab.com>
2024-04-18 18:31:19 +0200

    Comprueba next

--- controllers/auth.service.js
+++ controllers/auth.service.js
@@ -1,8 +1,9 @@
 
 const initController = require("../controllers/init.controller");
 exports.requireLogin = function(req, res, next) {
-    
+    console.log("requireLogin")
     if (req.session.loggedin) {
+        console.log("Sigue el login")
         next();
     } else {
         //res.send('Acceso no autorizado. Inicia sesión primero.');
