94837d64042b55d5ae29247f3e9d88219b575a1f
Mª Ángeles López <mariaangeles.lopez@hidralab.com>
2024-04-28 10:10:01 +0200

    Resolución de conflictos

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -8,28 +8,51 @@ async function getContactUsLogin(req,res){
     const repo = 'HidraSmart-CommonFiles';
     const rutaHeadLogin = 'components/head-login.html';
     const rutaBodyContactUs = 'components/contact-us.html';
-    
-    // Obtener el commit más reciente del archivo head-login.html
-    const commitShaHeadLogin = await obtenerCommitMasReciente(owner, repo, rutaHeadLogin);
 
-    // Construir la URL con la SHA del commit más reciente
-    const urlHeadLogin = `https://cdn.jsdelivr.net/gh/${owner}/${repo}@${commitShaHeadLogin}/${rutaHeadLogin}`;
+    let commitShaHeadLogin, commitShaBodyContactUs;
+    let urlHeadLogin, urlBodyContactUs;
+
+    try {
+      // Obtener el commit más reciente del archivo head-login.html
+      commitShaHeadLogin = await obtenerCommitMasReciente(owner, repo, rutaHeadLogin);
+    } catch (error) {
+        console.error('Error al obtener el commit más reciente:', error.message);
+    }
+
+    // Si se obtuvo correctamente el commit más reciente, construir la URL con la SHA del commit
+    if (commitShaHeadLogin) {
+        // Construir la URL con la SHA del commit más reciente
+        urlHeadLogin = `https://cdn.jsdelivr.net/gh/${owner}/${repo}@${commitShaHeadLogin}/${rutaHeadLogin}`;
+    } else {
+        // Si hubo un error o no se encontró ningún commit, intentar obtener el archivo directamente desde la rama principal
+        urlHeadLogin = `https://cdn.jsdelivr.net/gh/${owner}/${repo}@main/${rutaHeadLogin}`;
+    }
     
     // Obtener el contenido del archivo head-login.html
     const responseHeadLogin = await fetch(urlHeadLogin);
     const htmlHeadLogin = await responseHeadLogin.text();
 
-    // Obtener el commit más reciente del archivo body-login.html
-    const commitShaBodyContactUs = await obtenerCommitMasReciente(owner, repo, rutaBodyContactUs);
+    try {
+      // Obtener el commit más reciente del archivo body-login.html
+      commitShaBodyContactUs = await obtenerCommitMasReciente(owner, repo, rutaBodyContactUs);
+    } catch (error) {
+        console.error('Error al obtener el commit más reciente:', error.message);
+    }
 
-    // Construir la URL con la SHA del commit más reciente
-    const urlBodyContactUs = `https://cdn.jsdelivr.net/gh/${owner}/${repo}@${commitShaBodyContactUs}/${rutaBodyContactUs}`;
+    // Si se obtuvo correctamente el commit más reciente, construir la URL con la SHA del commit
+    if (commitShaBodyContactUs) {
+      // Construir la URL con la SHA del commit más reciente
+      urlBodyContactUs = `https://cdn.jsdelivr.net/gh/${owner}/${repo}@${commitShaBodyContactUs}/${rutaBodyContactUs}`;
+    } else {
+        // Si hubo un error o no se encontró ningún commit, intentar obtener el archivo directamente desde la rama principal
+        urlBodyContactUs = `https://cdn.jsdelivr.net/gh/${owner}/${repo}@main/${rutaBodyContactUs}`;
+    }
 
     // Obtener el contenido del archivo head-login.html
     const responseBodyContactUs = await fetch(urlBodyContactUs);
     const htmlBodyContactUs = await responseBodyContactUs.text();
     
-    res.render('/contact-us', {
+    res.render('contact-us', {
       headLoginHTML: htmlHeadLogin,
       bodyContactUsHTML: htmlBodyContactUs,
       title: 'Contacto' , 
