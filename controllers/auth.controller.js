75551e746029ea95a82a107f796501e53c6f50a1
Mª Ángeles López <mariaangeles.lopez@hidralab.com>
2024-05-02 10:00:09 +0200

    Últimas modificaciones para intentar refrescar la página

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,79 @@
-const authUtil = require("../utils/auth");
-const sessionFlash = require("../utils/session-flash");
+
+const fetch = require('node-fetch');
+const {RecaptchaEnterpriseServiceClient} = require('@google-cloud/recaptcha-enterprise');
+
+const {GoogleAuth} = require('google-auth-library');
 
 const { runQuery } = require("../data/bbdd-connector");
+
+
+async function verifyCaptcha(username, password, token){
+    const blobSasUrl = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+
+    const responseGoogle = await fetch(blobSasUrl);
+    const certContentGoogle = await responseGoogle.text();
+    
+    const auth = new GoogleAuth({
+        credentials: JSON.parse(certContentGoogle),
+        scopes: 'https://www.googleapis.com/auth/cloud-platform'
+    });
+    let client = new RecaptchaEnterpriseServiceClient({auth});
+    const projectID = "intense-emblem-415011";
+    const recaptchaKey = "6Lf3o3opAAAAAH0GHlp_LuajXdK_Ur8HCR8_vLqX";
+    const recaptchaAction = "LOGIN";
+  
+    const projectPath = client.projectPath(projectID);
+
+  // Crea la solicitud de evaluación.
+    const request = ({
+        assessment: {
+            event: {
+                token: token,
+                siteKey: recaptchaKey,
+            },
+        },
+        parent: projectPath,
+    });
+
+    const [ response ] = await client.createAssessment(request);
+
+    
+    if (!response.tokenProperties.valid || parseFloat(response.riskAnalysis.score) < 0.3) {
+        return null;
+    }
+    if (response.tokenProperties.action === recaptchaAction) {
+        // Obtén la puntuación de riesgo y los motivos.
+        // Para obtener más información sobre cómo interpretar la evaluación, consulta:
+        // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+        console.log(`The reCAPTCHA score is: ${response.riskAnalysis.score}`+parseFloat(response.riskAnalysis.score));
+        response.riskAnalysis.reasons.forEach((reason) => {
+            console.log(reason);
+        });
+        const queryString = `SELECT * FROM users WHERE username=? AND password=?;`;
+        const values = [username, password];
+        const database = 'hidrasmart_pn';
+        
+        try {
+            const result = await runQuery(queryString, values, database);
+        
+            if (result.success) {
+                const rows = result.data.rows;
+                return rows;
+            } else {
+                console.error('Error al ejecutar la consulta:', result.message);
+                // Maneja el caso de error según sea necesario
+            }
+        } catch (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            // Maneja el error lanzado por la función runQuery
+            throw error;
+        }
+    } else {
+        console.log("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
+        return null;
+    }
+}
+
 
 async function verifyUser(username, password, req) {
 
