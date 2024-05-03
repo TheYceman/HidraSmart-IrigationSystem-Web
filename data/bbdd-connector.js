544452240be179e05c9fc86017fd270b6bb93d48
Mª Ángeles López Novella <mariaangeles.lopez@hidralab.com>
2024-05-03 07:02:27 +0000

    Revert "Merged PR 106: Cambio Pool bbdd
    Cambio Pool bbdd"

--- data/bbdd-connector.js
+++ data/bbdd-connector.js
@@ -1,50 +1,40 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 const fetch = require('node-fetch');
 
 async function downloadBlob() {
-  //console.log('process.env.BLOB_SAS_URL:', process.env.BLOB_SAS_URL);
-  const blobSasUrl = process.env.BLOB_SAS_URL;
+    // console.log('process.env.BLOB_SAS_URL:', process.env.BLOB_SAS_URL);
+    const blobSasUrl = process.env.BLOB_SAS_URL
 
-  const response = await fetch(blobSasUrl);
-  const certContent = await response.text();
-  //console.log('Descargado el archivo .pem con éxito:\n', certContent);
-  return certContent;
+    const response = await fetch(blobSasUrl);
+    const certContent = await response.text();
+    // console.log('Descargado el archivo .pem con éxito:\n', certContent);
+    return certContent;
 }
 
 downloadBlob().catch((err) => {
-  console.error('Error al descargar el archivo .pem 3232:', err.message);
+    console.error('Error al descargar el archivo .pem:', err.message);
 });
 
-async function getDb(ddbb) {
-
-  const certContent = await downloadBlob();
-  console.log("bbb" + process.cwd());
-
-  try {
-
-    // Create the connection pool. The pool-specific settings are the defaults
-    const pool = mysql.createPool({
-      host: process.env.AZURE_MYSQL_HOST,
-      user: process.env.AZURE_MYSQL_USER,
-      password: process.env.AZURE_MYSQL_PASSWORD,
-      port: process.env.AZURE_MYSQL_PORT,
-      database: ddbb, //"aplicaciones_web" //Hay que meterla como argumento
-      waitForConnections: true,
-      connectionLimit: 10,
-      maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-      idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-      queueLimit: 0,
-      enableKeepAlive: true,
-      keepAliveInitialDelay: 0,
-      ssl: {
-        ca: certContent,
-      },
-    });
-
-    return pool;
-  } catch (err) {
-    console.error("Error base de datos getDb ", err);
-  }
+async function getDb(ddbb) {      
+    try {
+        const certContent = await downloadBlob();
+      
+        const conn = mysql.createConnection({
+            host: process.env.AZURE_MYSQL_HOST,
+            user: process.env.AZURE_MYSQL_USER,
+            password: process.env.AZURE_MYSQL_PASSWORD,
+            port:  process.env.AZURE_MYSQL_PORT,
+            database: ddbb,
+            ssl: {
+                ca: certContent,
+            },
+        });
+        console.log('Conexión exitosa a MySQL.');
+        return conn;
+    } catch (err) {
+        console.error('Error al conectar a MySQL:', err);
+        throw err;
+    }
 }
 
 async function runQuery(queryString, values, database) {
