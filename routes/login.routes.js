13da8707ea0fe4441b66dd7f44137a0d66f14437
TheYceman <101130438+TheYceman@users.noreply.github.com>
2025-02-13 13:23:10 +0100

    Update message contact us

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const loginController = require("../controllers/login.controller");
+const { verifyCaptcha } = require('../utils/verify-captcha');
 const emailSender = require('../utils/emailSender');
 
 const router = express.Router();
@@ -19,11 +20,42 @@ router.get("/change-password", loginController.getChangePasswordLogin);
 // Ruta para manejar el envío desde el formulario de contacto
 router.post('/send-email', async (req, res) => {
     const { token, contactname, contactemail, contactmessage } = req.body;
-    const emailSent = await emailSender.sendEmail(token,contactname, contactemail, contactmessage);
-    if (emailSent) {
-        res.status(200).json({ message: 'Correo electrónico enviado con éxito' });
-    } else {
-        res.status(500).json({ error: 'Error al enviar el correo electrónico' });
+
+    // Validar que el token esté presente
+    if (!token) {
+        return res.status(400).json({ 
+            success: false, 
+            message: "Falta el token en la solicitud."
+        });
+    }
+
+    try {
+        // Verificar el token con la función verifyCaptcha
+        const result = await verifyCaptcha(token);
+
+        if (!result || !result.success) {
+            console.error('Token inválido. Detalles:', result?.['error-codes'] || 'No disponible');
+            return res.status(400).json({ 
+                success: false, 
+                message: `Error: Token inválido. Detalles: ${result?.['error-codes'] || 'No disponible'}` 
+            });
+        }
+
+        // Intentar enviar el correo
+        const emailSent = await emailSender.sendEmail( contactname, contactemail, contactmessage);
+        
+        if (emailSent) {
+            return res.status(200).json({ success: true, message: 'Correo electrónico enviado con éxito' });
+        } else {
+            return res.status(500).json({ success: false, error: 'Error al enviar el correo electrónico' });
+        }
+
+    } catch (error) {
+        console.error('Error en el proceso de envío de correo:', error);
+        return res.status(500).json({ 
+            success: false, 
+            message: 'Error interno del servidor al enviar el correo electrónico.'
+        });
     }
 });
 
--- utils/emailSender.js
+++ utils/emailSender.js
@@ -18,12 +18,12 @@ const transporter = nodemailer.createTransport({
 
 
 // Función para enviar el correo electrónico
-async function sendEmail(token, contactname, contactemail, contactmessage) {
+async function sendEmail( contactname, contactemail, contactmessage) {
     try {
         let mailOptions = {
             from: process.env.USER_GMAIL,
             to: contactemail,
-            bcc: 'carlos.merino@hidralab.com, hidrasmarthydraulicinnovation@hotmail.com, emilio.ruiz@hidralab.com, alejandro.quintana@hidralab.com',
+            bcc: 'carlos.merino@hidralab.com, hidrasmarthydraulicinnovation@hotmail.com, emilio.ruiz@hidralab.com',
             subject: 'Consulta Irrigation System',
             text: `Nombre: `+contactname+`\nEmail: `+contactemail+`\nMensaje: `+contactmessage
         };
@@ -88,56 +88,5 @@ async function sendEmailNewUser(nombre, email, claveGenerada) {
     }
   
   };
-
-async function verifyCaptcha(username, password, token){
-    const credentialsJSON = JSON.parse(fs.readFileSync('./data/intense-emblem-415011-b892b5269f66.json'));//fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON, 'utf8'));
-    const auth = new GoogleAuth({
-        credentials: credentialsJSON,
-        scopes: 'https://www.googleapis.com/auth/cloud-platform'
-    });
-    let client = new RecaptchaEnterpriseServiceClient({auth});
-    const projectID = "intense-emblem-415011";
-    const recaptchaKey = "6Lf3o3opAAAAAH0GHlp_LuajXdK_Ur8HCR8_vLqX";
-    const recaptchaAction = "CONTACT_US";
-  
-    const projectPath = client.projectPath(projectID);
-
-  // Crea la solicitud de evaluación.
-    const request = ({
-        assessment: {
-            event: {
-                token: token,
-                siteKey: recaptchaKey,
-            },
-        },
-        parent: projectPath,
-    });
-
-    const [ response ] = await client.createAssessment(request);
-
-    
-    if (!response.tokenProperties.valid) {
-        return null;
-    }
-    if (response.tokenProperties.action === recaptchaAction) {
-        // Obtén la puntuación de riesgo y los motivos.
-        // Para obtener más información sobre cómo interpretar la evaluación, consulta:
-        // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
-        console.log(`The reCAPTCHA score is: ${response.riskAnalysis.score}`);
-        response.riskAnalysis.reasons.forEach((reason) => {
-            console.log(reason);
-        });
-
-        const queryString = `SELECT * FROM users WHERE username=? AND password=?;`;
-        const values = [username,password];
-        const database = 'aplicaciones_web';
-        const [results, fields] = await runQuery(queryString, values, database);
-
-        return results;//response.riskAnalysis.score;
-    } else {
-        console.log("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
-        return null;
-    }
-}
 
 module.exports = { sendEmail, sendEmailNewUser };
