ec00442376c4b4ee2b908acb74cd2ece24df1f4b
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-06-12 13:24:23 +0200

    fix: comentarios

--- frontend/src/components/api-keys/GoogleApiKeyProvider.jsx
+++ frontend/src/components/api-keys/GoogleApiKeyProvider.jsx
@@ -3,8 +3,10 @@ import React, { useEffect } from "react";
 const GoogleApiKeyProvider = () => {
   useEffect(() => {
     // Obtener la API key de la variable de entorno en .env
+    // IMPORTANTE la variable debe empezar por VITE_
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
     
+    // Asignar la API key a la variable global dentro de la ventana en la que se ejecute
     window.VITE_GOOGLE_MAPS_API_KEY = apiKey;
 
   }, []);
