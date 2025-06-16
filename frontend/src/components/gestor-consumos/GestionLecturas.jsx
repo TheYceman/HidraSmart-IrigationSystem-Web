a5a3c580873b812bfec54c9145f183b6e21cbc16
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-06-16 17:22:53 +0200

    update: primera ruta gestion lecturas

--- frontend/src/components/gestor-consumos/GestionLecturas.jsx
+++ frontend/src/components/gestor-consumos/GestionLecturas.jsx
@@ -28,62 +28,73 @@ function GestionLecturas() {
     return (
 
         <div className={styles.contenedor_principal}>
-            <div className={`${styles.peticiones_lectura} ${styles.filtros_tabla}`}>
-                <div className={styles.filtros}>
-                    <div>
-                        <label htmlFor="periodo-select-1">Seleccione día</label>
-                        <input type="datetime-local" id="periodo-select-1" name="fecha" />
+            <div className={`${styles.peticiones_lectura}`}>
+                <div className={`${styles.titulo_pestania}`}>PETICIONES</div>
+                <div className={styles.filtros_tabla}>
+                    <div className={styles.filtros}>
+                        <div>
+                            <label htmlFor="periodo-select-1">Seleccione día</label>
+                            <input type="datetime-local" id="periodo-select-1" name="fecha" />
+                        </div>
+                        <div>
+                            <label htmlFor="sector-select">Balsa</label>
+                            <select name="sector" id="sector-select">
+                                <option value="todos">Todas</option>
+                                <option value="1">Balsa 1</option>
+                                <option value="2">Balsa 2</option>
+                                <option value="3">Balsa 3</option>
+                                <option value="4">Balsa 4</option>
+                                <option value="5">Balsa 5</option>
+                                <option value="6">Balsa 6</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label htmlFor="contador-select">Contador</label>
+                            <select name="contador" id="contador-select">
+                                <option value="0CM0201">Todos</option>
+                                <option value="0CM0201">0CM0201</option>
+                                <option value="0CM0202">0CM0202</option>
+                                <option value="0CM0203">0CM0203</option>
+                            </select>
+                        </div>
+                        <button><i className="fas fa-folder-open"></i>Exportar a Excel</button>
                     </div>
-                    <div>
-                        <label htmlFor="sector-select">Balsa</label>
-                        <select name="sector" id="sector-select">
-                            <option value="todos">Todos</option>
-                            <option value="sector1">Sector 1</option>
-                            <option value="sector2">Sector 2</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="contador-select">Contador</label>
-                        <select name="contador" id="contador-select">
-                            <option value="0CM0201">Todos</option>
-                            <option value="0CM0201">0CM0201</option>
-                            <option value="0CM0202">0CM0202</option>
-                            <option value="0CM0203">0CM0203</option>
-                        </select>
-                    </div>
-                    <button><i className="fas fa-folder-open"></i>Exportar a Excel</button>
-                </div>
-                <div className="custom_scrollbar">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Fecha Hora</th>
-                                <th>Nombre</th>
-                                <th>Pedidor</th>
-                                <th>Asignado</th>
-                                <th>Prioridad</th>
-                                <th>Estado</th>
-                                <th>Tipo</th>
-                                <th>Comentarios</th>
-                                <th>Acción</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Array.from({ length: 14 }).map((_, i) => (
-                                <tr key={i}>
-                                    <td>Dato 1</td>
-                                    <td>Dato 2</td>
-                                    <td>Dato 3</td>
-                                    <td>Dato 4</td>
-                                    <td>Dato 5</td>
-                                    <td>Dato 6</td>
-                                    <td>Dato 7</td>
-                                    <td>Dato 8</td>
-                                    <td><i className="fas fa-edit"></i></td>
+                    <div className={`${styles.contenedor_scroll} custom_scrollbar`}>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Fecha</th>
+                                    <th>Nombre</th>
+                                    <th>Pedidor</th>
+                                    <th>Asignado</th>
+                                    <th>Prioridad</th>
+                                    <th>Estado</th>
+                                    <th>Tipo</th>
+                                    <th>Comentarios</th>
+                                    <th></th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {Array.from({ length: 14 }).map((_, i) => (
+                                    <tr key={i}>
+                                        <td>Dato 1</td>
+                                        <td>Dato 2</td>
+                                        <td>Dato 3</td>
+                                        <td>Dato 4</td>
+                                        <td>Dato 5</td>
+                                        <td>Dato 6</td>
+                                        <td>Dato 7</td>
+                                        <td>
+                                            <span onClick={openImageLectura} className={styles.ver_mas}>
+                                                <i className="fas fa-align-left"></i>Ver más
+                                            </span>
+                                        </td>
+                                        <td><i className="fas fa-edit"></i></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
 
@@ -94,7 +105,8 @@ function GestionLecturas() {
             </div>
 
             <div className={`${styles.lecturas}`}>
-                <div className="custom_scrollbar">
+                <div className={`${styles.titulo_pestania}`}>LECTURAS DE CONTADORES</div>
+                <div className={`${styles.contenedor_scroll} custom_scrollbar`}>
                     <table>
                         <thead>
                             <tr>
@@ -103,7 +115,7 @@ function GestionLecturas() {
                                 <th>Usuario</th>
                                 <th>Volumen (m3)</th>
                                 <th>Imagen</th>
-                                <th>Accion</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -118,7 +130,7 @@ function GestionLecturas() {
                                         ref={fileInputRef}
                                         style={{ display: 'none' }}
                                     />
-                                    <button onClick={handleFileButtonClick}>Seleccionar archivo</button>
+                                    <button onClick={handleFileButtonClick}>Subir archivo</button>
                                 </td>
                                 <td><i className="fas fa-plus"></i></td>
                             </tr>
@@ -129,7 +141,7 @@ function GestionLecturas() {
                                     <td>Dato 3</td>
                                     <td>Dato 4</td>
                                     <td>
-                                        <span onClick={openImageLectura}>
+                                        <span onClick={openImageLectura} className={styles.ver_mas}>
                                             <i className="fas fa-image"></i>Ver imagen
                                         </span>
                                     </td>
@@ -152,13 +164,11 @@ function GestionLecturas() {
                 title="Imagen de lectura"
                 width="600px"
                 height="400px"
-                footer={<button onClick={() => setIsImagePopupOpen(false)}>Cerrar</button>}
             >
                 <div style={{ textAlign: "center" }}>
                     <img src="https://via.placeholder.com/300" alt="Imagen seleccionada" />
                 </div>
             </PopupConfig>
-
         </div>
     );
 }
